2763befc2f472fe3ed6edbdb716cef1e8abb627c
Shola Eyinmosan <novertech4@gmail.com>
2021-10-22 07:47:17 -0700

    get files

--- script.js
+++ script.js
@@ -14,12 +14,30 @@ async function getPriceFeed() {
 
         const $ = cheerio.load(data)
         const elemSelector ='#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr'
-        
+        const Keys = [
+            'rank',
+            'name',
+            'price',
+            '24h',
+            '7d',
+            'marketCap',
+            'volume',
+            'circulatingSupply'
+
+
+        ]
         
         $(elemSelector).each((parentIdx, parentElem) =>  {
+            let KeyIdx = 0;
              if (parentIdx <= 9) {
                  $(parentElem).children().each((childIdx, childElem) =>{
-                     console.log($(childElem).text())
+                     const tdValue = $(childElem).text()
+
+                     if (tdValue){
+                       console.log(Keys[KeyIdx])
+
+                       KeyIdx++
+                     }
                  })
              }
         });

a9c070b4ad71e588aac0df9fc4845fcb44fd6aa9
Shola Eyinmosan <novertech4@gmail.com>
2021-10-26 07:08:28 -0700

    created an if statment  that seleted only the currency name and values but leaves the tegar  out of it

--- script.js
+++ script.js
@@ -25,30 +25,59 @@ async function getPriceFeed() {
             'circulatingSupply'
 
         ]
-        
+
+        const coinArr = []
+
         $(elemSelector).each((parentIdx, parentElem) =>  {
-            let KeyIdx = 0;
-            const coinObj = {};
+            let KeyIdx = 0
+            const coinObj = {}
 
              if (parentIdx <= 9) {
                  $(parentElem).children().each((childIdx, childElem) =>{
-                     const tdValue = $(childElem).text()
+                     let tdValue = $(childElem).text()
+
+
+                     if (KeyIdx ===  1 || KeyIdx === 6) {
+                         tdValue = $('p:first-child', $(childElem).html()).text()
+                     }
 
                      if (tdValue){
-                       coinObj(Keys[KeyIdx]) = tdValue 
+                       coinObj[Keys[KeyIdx]] = tdValue 
 
                        KeyIdx++
                      }
                  })
+                 coinArr.push(coinObj)
              }
-             console.log(coinObj)
-        });   
-
+            
+        })  
+           return(coinArr)
     } catch (err) {
     console.error(err)
     }
 }
 
 
+const app = express()
 
-getPriceFeed()
+app.get('/api/price-feed', async (req, res) => {
+try{
+const priceFeed = await getPriceFeed()
+
+return res.status(200).json({
+    result: priceFeed
+})
+
+} catch (err){
+    return res.status(500).json({
+        err:err.toString(),
+    })
+}
+
+
+})
+
+
+app.listen(3000, () => {
+ console.log("running on port 3000")
+})
